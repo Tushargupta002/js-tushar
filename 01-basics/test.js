5e0994f556f5afd6481b33774eaaec1804556fc0
Tushargupta002 <montualg123@gmail.com>
2024-08-20 23:22:34 +0000

    variable change

--- 01-basics/test.js
+++ 01-basics/test.js
@@ -1,1 +1,6 @@
 console.log("Hello")
+const accountId = 169;
+console.log(accountId);
+let accountCity = "Aligarh";
+var accountPassword = 1234;
+console.table([accountId,accountCity,accountPassword]);
