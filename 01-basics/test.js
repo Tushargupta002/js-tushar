7fd77f14a3a41bea77aae3bd1fec6b7fb3b5d117
Tushargupta002 <montualg123@gmail.com>
2024-08-21 02:11:17 +0000

    variables

--- 01-basics/test.js
+++ 01-basics/test.js
@@ -1,6 +1,15 @@
-console.log("Hello")
-const accountId = 169;
-console.log(accountId);
-let accountCity = "Aligarh";
-var accountPassword = 1234;
-console.table([accountId,accountCity,accountPassword]);
+// console.log("Hello")
+// const accountId = 169;
+// console.log(accountId);
+// let accountCity = "Aligarh";
+// var accountPassword = 1234;
+// console.table([accountId,accountCity,accountPassword]);
+// console.log(typeof null);
+
+//"33" = 33;
+//"33abc" =>  Nan;
+//true => 1 ; false => 0;
+let loggedIn = undefined;
+let boolean = Number(loggedIn);
+console.log(boolean);
+
